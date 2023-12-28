128423241886d1cc1954b8825ab5fcd3a035e84f
Hassan Mehmood <hassanafridi1913@gmail.com>
2023-12-28 15:47:10 +0500

    feat: db setup and basic insertion done

--- app/actions.ts
+++ app/actions.ts
@@ -1,7 +1,15 @@
 'use server';
 
 import { RegisterInputs } from './login/RegisterUser';
+import prisma from '@/db';
 
 export async function registerUser(formData: RegisterInputs) {
-  console.log(formData);
+  const newUser = await prisma.user.create({
+    data: {
+      username: formData.username,
+      email: formData.email,
+      password: formData.password,
+      confirmPass: formData.confirmPass,
+    },
+  });
 }
