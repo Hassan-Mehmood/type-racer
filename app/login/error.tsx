d1fccf20b7b63acc64e88814ad9839e0e7fcfddb
Hassan Mehmood <hassanafridi1913@gmail.com>
2024-01-06 12:43:03 +0500

    refactor: fixed issues in the register user

---
+++ app/login/error.tsx
@@ -0,0 +1,23 @@
+'use client' // Error components must be Client Components
+
+export default function Error({
+	reset,
+}: {
+	reset: () => void
+}) {
+
+	return (
+		<div>
+			<h2>Something went wrong!</h2>
+			<button
+
+				onClick={
+
+					() => reset()
+				}
+			>
+				Try again
+			</button>
+		</div>
+	)
+}
--- lib/schema.ts
+++ lib/schema.ts
@@ -11,3 +11,10 @@ export const RegisterUserSchema = z
     message: "Passwords don't match",
     path: ['confirmPass'],
   });
+
+// .object({
+//   username: z.string().min(1, 'Username is required'),
+//   email: z.string().email('Invalid eamil address'),
+//   password: z.string().min(6, 'Password must be greated than 6 characters'),
+//   confirmPass: z.string().min(6, 'Password must be greated than 6 characters'),
+// })
